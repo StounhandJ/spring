d96adb83c03ea383a8a7ef3d0aff5239fa9ce95e
Roman Marinichev <rmarinicev@gmail.com>
2022-10-23 14:17:54 +0300

    restrictions for BookController

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -8,6 +8,7 @@ import com.example.demo.repo.BookRepository;
 import com.example.demo.repo.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.repo.PostRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("book")
+@PreAuthorize("hasAnyAuthority('USER')")
 public class BookController {
     @Autowired
     private BookRepository bookRepository;

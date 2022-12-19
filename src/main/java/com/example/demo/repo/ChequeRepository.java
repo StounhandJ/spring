1b4f56be4643d57e0252e5745269f9947356887c
Roman Marinichev <rmarinicev@gmail.com>
2022-12-19 22:29:01 +0300

    native query

--- src/main/java/com/example/demo/repo/ChequeRepository.java
+++ src/main/java/com/example/demo/repo/ChequeRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.repo;
 
 import com.example.demo.models.Cheque;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ChequeRepository extends CrudRepository<Cheque, Long> {
 
+    @Modifying
+    @Query(value = "SELECT cheque.* FROM cheque JOIN paid_treatment ON paid_treatment.id=cheque.paid_treatment_id JOIN application ON application.id=paid_treatment.application_id WHERE application.client_id=?;", nativeQuery = true)
     List<Cheque> findByPaidTreatmentApplicationClient_Id(long id);
 
 }
--- src/main/java/com/example/demo/repo/PaidTreatmentRepository.java
+++ src/main/java/com/example/demo/repo/PaidTreatmentRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.repo;
 
 import com.example.demo.models.PaidTreatment;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PaidTreatmentRepository extends CrudRepository<PaidTreatment, Long> {
 
+    @Modifying
+    @Query(value = "SELECT paid_treatment.* FROM paid_treatment JOIN application ON application.id=paid_treatment.application_id WHERE application.client_id=?;", nativeQuery = true)
     List<PaidTreatment> findByApplicationClient_Id(long id);
 
 }
